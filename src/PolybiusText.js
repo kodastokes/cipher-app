2c3cc52f66f8af5ef5b0a9293237383be0b38a8b
Koda Stokes <kodastokes11@gmail.com>
2022-05-05 20:13:30 -0600

    edited polybius text

--- src/PolybiusText.js
+++ src/PolybiusText.js
@@ -3,7 +3,7 @@ function PolybiusText() {
       <>
         <p></p>
         <p>
-        A  Polybius Square cipher uses a 5x5 table to convert letters into number pairs. In order to fit all letters of the English alphabet into the 25 cell table, the letters "i" and "j" are combined and will display as "i/j." To decode successfully, each letter must be given as a valid Polybius number pair.
+        A  Polybius Square cipher uses a 5x5 table to convert letters into number pairs. Ex: A Polybius Square will shift A to 11, M to 23, and X to 35. In order to fit all letters of the English alphabet into the 25 cell table, the letters "I" and "J" are combined and will display as "I/J." To decode successfully, each letter must be given as a valid Polybius number pair.
         </p>
       </>
     );
